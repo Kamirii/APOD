368a164f0a0a82594382db59d00338a3be34ca70
Kamirii <camilyaalbres@gmail.com>
2022-05-25 18:10:28 -0300

    infoAPI aparecendo na tela

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
-  
+
 //`https://api.nasa.gov/planetary/apod?api_key=fCzg8UWG84gFSAfB15ozm0RlosaE8BMuwbLm8bMQ`
-//`https://api.nasa.gov/planetary/apod?api_key=fCzg8UWG84gFSAfB15ozm0RlosaE8BMuwbLm8bMQ` + $('#submit').val(),
 
 
+// const data  = $(`#data-apod`).val() 
 
-  $("#submit").on ('click', function()
-{
+document.querySelector("#submit").addEventListener("click", () => {});
+
+  $("#submit").on ('click', function(e) { 
+  const data  = $(`#data-apod`).val() 
   $.ajax({
     // "?" define o começo de um query parameter
-    url: `https://api.nasa.gov/planetary/apod?api_key=fCzg8UWG84gFSAfB15ozm0RlosaE8BMuwbLm8bMQ`,
+    url: `https://api.nasa.gov/planetary/apod?api_key=fCzg8UWG84gFSAfB15ozm0RlosaE8BMuwbLm8bMQ&date=${data}`,
     type: 'GET',
     //dataType
-    success: function (osta) {
-      console.log(osta);
-    },
-    // error: function (error){
-    //   console.log(error)
-    // }
-     });
-  }); 
+    success: function (apod) {
+      const infoImagem = $(".texto");
+      const diaDaImagem = $(".dia");
 
-    
+      diaDaImagem.html(`${apod.date}`)
+      infoImagem.html(`${apod.explanation}`);
+      console.log(apod.explanation)
+      console.log(apod.date)
+     
+          }, 
+        }) 
+     });
+ 
+ //arrumar botao 
+ //arrumar display
+ //separar por funcões?
+ //tratamento de erro
+ 
+
 
